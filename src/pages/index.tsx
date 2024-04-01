6e87ffe31b9b53b25116b52678b5a8ea2db86e7f
Eden <ecoding@pm.me>
2024-04-01 14:17:48 -0700

    Added a Prizes Page
    1. Changed the name to be 3.5 and not 3
    
    2. Added Prizes to the index
    
    3. Added a Prizes.tsx page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Contact, Faq, Info, PastWinners, Pillars, Sponsors, Team} from "@/views"
+import { Prizes } from "@/views/Prizes"
 
 export default function Home() {
   return (
     <div className="bg-gradient-to-b from-mhblue via-mhpink-200 to-mhpurple-100">
       <Info />
       <Pillars />
+      <Prizes />
       <PastWinners />
       <Team />
       <Sponsors />
