7fa5e14853ab8226631a8e52bdd976c15a3b553d
Eden <ecoding@pm.me>
2024-04-03 22:17:11 -0700

    Finished Prizes

--- src/views/Prizes.tsx
+++ src/views/Prizes.tsx
@@ -1,41 +1,47 @@
-import { FaRegHandshake, FaRegHeart, FaTv } from "react-icons/fa"
+import Link from 'next/link'
+import Image from 'next/image'
 
 export const Prizes = () => {
   return (
-    <div id="prizes" className="flex flex-col justify-center items-center pt-12">
-      <h1 className="text-6xl font-bold text-black opacity-50 mt-20 mb-6">
-        Prizes
-      </h1>
-      <p className="text-2xl font-bold text-black opacity-50">
-        Win one of these awesome prizes at Marina Hacks!
-      </p>
-      <div className="flex flex-row justify-center items-center gap-6 my-10">
-        <div className="w-96 flex flex-col justify-center items-center rounded-2xl p-6 bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
-          <FaRegHandshake size='10rem' />
-          <h1 className="text-3xl my-6">
-            PRIZE 1
-          </h1>
-          <p className="text-1xl text-center">
-            {"Social distancing & virtual learning doesn't mean we should be disconnected. Make technology that can bring us together."}
-          </p>
+    <div id="prizes" className="flex flex-col justify-center items-center pt-16 gap-16">
+      <div className="flex justify-center items-center">
+        <h1 className="text-6xl font-extrabold text-[#0F516F] mt-20 mb-2 ">
+          Prizes
+        </h1>
+      </div>
+      {/* Flex container for the jellyfish images and text */}
+      <div className="flex justify-center items-center gap-10">
+        {/* 2020 Winners */}
+        <div className="relative">
+          <Image
+            src={"/images/Prize_Jellyfish.png"}
+            alt="First Prize"
+            width="450"
+            height="450"
+            text = "First class entertainment speakers!"
+          />
+    
         </div>
-        <div className="w-96 flex flex-col justify-center items-center rounded-2xl p-6 bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
-          <FaRegHeart size='10rem' />
-          <h1 className="text-3xl my-6">
-            PRIZE 2
-          </h1>
-          <p className="text-1xl text-center">
-          {"COVID-19 has changed the lives of many in our communities. Create technology that can improve our own well-being, as well as the environment's."}
-          </p>
+
+        <div className="relative">
+          <Image
+            src={"/images/Prize_Jellyfish.png"}
+            alt="Second Prize"
+            width="450"
+            height="450"
+            text = "Best in class mouse!"
+          />
         </div>
-        <div className="w-96 flex flex-col justify-center items-center rounded-2xl p-6 bg-gradient-to-r from-mhpurple-200 to-mhpurple-400">
-          <FaTv size='10rem' />
-          <h1 className="text-3xl my-6">
-            PRIZE 3
-          </h1>
-          <p className="text-1xl text-center">
-          Come join the fun!!!
-          </p>
+
+        <div className="relative">
+          <Image
+            src={"/images/Prize_Jellyfish.png"}
+            alt="Third Prize"
+            width="450"
+            height="450"
+            text = "Hacker approved mousepad!"
+          />
+
         </div>
       </div>
     </div>
